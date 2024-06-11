5038daa9dca95980df8c32a6de848884d06a83fb
Alexis Valentin <alexis.valentin-ext@mappy.com>
2024-06-11 17:37:00 +0200

    config: setup material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { TimerType } from "./consts/form";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 
 type Inputs = {
   timerLabel: string;
@@ -20,17 +27,28 @@ const App = () => {
       <h2>Timers and intervals manager progressive webapp</h2>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="timerLabel">Timer name</label>
-        <input {...register("timerLabel", { required: true })} />
-        <label htmlFor="timerType">Timer type</label>
-        <select {...register("timerType", { required: true })}>
-          <option value={TimerType.classic}>Classic</option>
-          <option value={TimerType.countdown}>Countdown</option>
-          <option value={TimerType.interval}>Interval</option>
-        </select>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <TextField
+            id="timerLabel"
+            label="Timer name"
+            variant="standard"
+            {...register("timerLabel", { required: true })}
+          />
+        </FormControl>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="timerType">Timer type</InputLabel>
+          <Select
+            labelId="timeType"
+            {...register("timerType", { required: true })}
+          >
+            <MenuItem value={TimerType.classic}>Classic</MenuItem>
+            <MenuItem value={TimerType.countdown}>Countdown</MenuItem>
+            <MenuItem value={TimerType.interval}>Interval</MenuItem>
+          </Select>
 
-        {errors.timerLabel && <span>Timer name is required</span>}
-        <input type="submit" />
+          {errors.timerLabel && <span>Timer name is required</span>}
+          <input type="submit" />
+        </FormControl>
       </form>
     </>
   );
